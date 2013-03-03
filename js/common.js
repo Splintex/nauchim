b87308608a92c3668d228316407a6ff5fba25b2c
Splintex <godun.yevgen@gmail.com>
2013-03-03 17:47:42 +0200

    DONE!

--- js/common.js
+++ js/common.js
@@ -1,3 +1,13 @@
 $(document).ready(function() {
-
+	$(".btns .btn_blue").click(function(){
+		if ($(this).hasClass("js-active")) {
+			$(this).removeClass("js-active");
+			$(this).children("ul").slideUp();
+		}
+		else {
+			$(this).addClass("js-active");
+			$(this).children("ul").slideDown();
+		}
+		return false;
+	});
 });
